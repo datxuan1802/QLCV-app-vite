72badc633b148e76747d4fc90f5ec04d2096477b
datxuan1802 <phamxuandat18022002@gmail.com>
2025-02-27 11:53:50 +0700

    fix icon user

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,16 +6,16 @@ interface AvatarProps {
 }
 
 export const AvatarCus = ({ user, tailwind, ...rest }: AvatarProps) => {
-  if (!user?.avatar)
-    return (
-      <Avatar className={`${tailwind} bg-blue-400`}>
-        {user?.name?.charAt(0).toUpperCase()}
-      </Avatar>
-    );
+  // if (!user?.avatar)
   return (
-    <Avatar
-      className={`w-6 h-6 flex bg-blue-200 items-center justify-center text-xs ${tailwind}`}
-      src={user?.avatar}
-    ></Avatar>
+    <Avatar className={`${tailwind} bg-blue-400`}>
+      {user?.name?.charAt(0).toUpperCase()}
+    </Avatar>
   );
+  // return (
+  //   <Avatar
+  //     className={`w-6 h-6 flex bg-blue-200 items-center justify-center text-xs ${tailwind}`}
+  //     src={user?.avatar}
+  //   ></Avatar>
+  // );
 };
