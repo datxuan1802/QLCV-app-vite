12816d661bafeafa9741ac9f31ffe539756756bf
datxuan1802 <phamxuandat18022002@gmail.com>
2025-02-27 12:30:28 +0700

    fix home page, logout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,5 +1,5 @@
 // components
-import { Layout, Menu, Avatar, Input, Button } from "antd";
+import { Layout, Menu, Avatar, Input, Button, Modal } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import {
   AddBoardTab,
@@ -24,6 +24,7 @@ import { userAtom } from "@/states/user.state";
 import { useNavigate } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { get } from "@/services/axios.service";
+import { useState } from "react";
 
 const SettingsTab = () => {
   const navigation = useNavigate();
@@ -117,42 +118,74 @@ export const MainHeader = () => {
       });
     },
   });
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    localStorage.clear();
+    navigation("/login");
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <Header className="z-50 bg-blue-400 flex flex-row justify-center items-start space-x-2 fixed top-0 left-0 right-0">
-      <div className="flex items-center justify-center mt-1">
-        <img src="/leadership.png" alt="" className="w-10 h-10" />
-      </div>
-      <div className="text-2xl text-bold text-center mt-3">Task management</div>
-      {/* <div>Workspace</div>
+    <>
+      <Modal
+        title="Xác nhận đăng xuất"
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="Đăng xuất"
+        cancelText="Hủy"
+        okButtonProps={{
+          className:
+            "hover:bg-blue-600 hover:text-white hover:border-none text-black border border-black ",
+        }}
+        cancelButtonProps={{
+          className:
+            "border border-black text-black hover:bg-red-100 hover:text-red-500 hover:border-none",
+        }}
+      >
+        Bạn có chắc chắn muốn đăng xuất không?
+      </Modal>
+      <Header className="z-50 bg-blue-400 flex flex-row justify-center items-start space-x-2 fixed top-0 left-0 right-0">
+        <div className="flex items-center justify-center mt-1">
+          <img src="/leadership.png" alt="" className="w-10 h-10" />
+        </div>
+        <div className="text-2xl text-bold text-center mt-3">
+          Task management
+        </div>
+        {/* <div>Workspace</div>
       <div>template</div>
       <div>create</div> */}
-      <div className="flex justify-end items-center flex-1 space-x-2">
-        <Search
-          placeholder="input search text"
-          //    onSearch={onSearch}
-          enterButton
-          className="w-56"
-        />
-        <div>
-          {!isLoading && (
-            <Avatar
-              style={{ backgroundColor: "#87d068" }}
-              icon={<UserOutlined />}
-              src={data?.avatar}
-            />
-          )}
-          {/* {user && <AvatarCus user={user} />} */}
+        <div className="flex justify-end items-center flex-1 space-x-2">
+          <Search
+            placeholder="input search text"
+            //    onSearch={onSearch}
+            enterButton
+            className="w-56"
+          />
+          <div className="flex items-center gap-x-1">
+            {!isLoading && (
+              <Avatar
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+                src={data?.avatar}
+              />
+            )}
+            {!isLoading && <div>{data?.name}</div>}
+            {/* {user && <AvatarCus user={user} />} */}
+          </div>
+          <Button type="text" onClick={showModal}>
+            Logout
+          </Button>
         </div>
-        <Button
-          type="text"
-          onClick={() => {
-            localStorage.clear();
-            navigation("/login");
-          }}
-        >
-          Logout
-        </Button>
-      </div>
-    </Header>
+      </Header>
+    </>
   );
 };
